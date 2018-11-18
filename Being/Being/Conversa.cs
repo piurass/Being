3a5ed9252098250783f53ca3a551e7dcd7844169
Rogerio de Alencar Santos <piurass@yahoo.com.br>
2018-11-18 01:22:40 -0200

    Adicionar arquivos de projeto.

---
+++ Being/Being/Conversa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Being
+{
+    class Conversa
+    {
+        public string Mensagem { get; set; }
+        public string Resposta { get; set; }
+        public void getRespostaBot()
+        {
+            Resposta="Eu ainda não penso, mas já existo!";
+        }
+    }
+
+}
---
+++ Being/Being/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Being
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Enviar_Clicked(object sender, EventArgs e)
+        {
+            var conversa = new Conversa
+            {
+                Mensagem = Mensagem.Text
+            };
+
+            Mensagem.Text = "";
+            MinhaMensagem.Text = conversa.Mensagem;
+            conversa.getRespostaBot();
+            Resposta.Text = conversa.Resposta;
+        }
+    }
+}
